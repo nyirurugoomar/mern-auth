19696e7cdcbcc8cd3f56fd6550d2ee4758f3e266
nyirurugo omar <nyirurugoomar@gmail.com>
2024-01-22 20:58:31 +0200

    create signup api router

--- api/index.js
+++ api/index.js
@@ -1,5 +1,8 @@
 import express from "express";
 import mongoose from "mongoose";
+import userRoutes from "./routes/user.route.js";
+import authRoutes from "./routes/auth.route.js";
+
 import dotenv from "dotenv";
 
 dotenv.config();
@@ -16,6 +19,11 @@ mongoose
 const port = 3000;
 const app = express();
 
+app.use(express.json());
+
 app.listen(port, () => {
   console.log(`The server is running on ${port} `);
 });
+
+app.use("/api/user", userRoutes);
+app.use("/api/auth", authRoutes);
