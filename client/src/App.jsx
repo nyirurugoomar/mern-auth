15a88773b67a25a2da4473576795cc11b3fac1d2
nyirurugo omar <nyirurugoomar@gmail.com>
2024-01-22 14:58:19 +0200

    add router and create pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,23 @@
 // eslint-disable-next-line no-unused-vars
-import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import Profile from "./pages/Profile";
 
 function App() {
-  return <div>App</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
