680358283747cc80b35195078a9bcef5c49cdb1d
nyirurugo omar <nyirurugoomar@gmail.com>
2024-02-20 20:30:36 +0200

    adding created api on home page

--- api/models/card.model.js
+++ api/models/card.model.js
@@ -1,24 +1,27 @@
 import mongoose from "mongoose";
 
-const cardSchema = new mongoose.Schema({
-  title: {
-    type: String,
-    required: true,
+const cardSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    service: {
+      type: String,
+      required: true,
+    },
+    ssdNo: {
+      type: String,
+      required: true,
+    },
+    companyProfilePicture: {
+      type: String,
+      default:
+        "https://cdn.vectorstock.com/i/preview-1x/65/30/default-image-icon-missing-picture-page-vector-40546530.jpg",
+    },
   },
-  service: {
-    type: String,
-    required: true,
-  },
-  ssdNo: {
-    type: String,
-    required: true,
-  },
-  companyProfilePicture: {
-    type: String,
-    default:
-      "https://cdn.vectorstock.com/i/preview-1x/65/30/default-image-icon-missing-picture-page-vector-40546530.jpg",
-  },
-});
+  { timestamps: true }
+);
 
 const Card = mongoose.model("Card", cardSchema);
 
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,8 +1,73 @@
 // eslint-disable-next-line no-unused-vars
-import React from "react";
+import React, { useEffect, useState } from "react";
+
+import axios from "axios";
 
 function Home() {
-  return <div>Home</div>;
+  const [cards, setCards] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchCards = async () => {
+      try {
+        const response = await axios.get(
+          "http://localhost:5173/api/card/getCards"
+        );
+        setCards(response.data);
+        setLoading(false);
+      } catch (error) {
+        console.error("Error fetching cards:", error);
+        setLoading(false);
+      }
+    };
+
+    fetchCards();
+  }, []);
+  return (
+    <div>
+      <div className="bg-blue-800 p-20 w-full ">
+        <h1 className="text-center text-white">Home page</h1>
+        <h1 className="text-center text-white mt-4">Search.....input</h1>
+      </div>
+      <div className="w-full h-full">
+        {loading ? (
+          <div className="text-center mt-10">
+            <p>Loading...</p>
+          </div>
+        ) : (
+          <div className="mt-10">
+            <div className="grid grid-cols-1 lg:grid-cols-3 gap-10 gap-y-16 mx-10 rounded-sm mb-40">
+              {cards.map((card, index) => (
+                <div
+                  key={index}
+                  className="bg-white shadow-2xl rounded-xl px-2"
+                >
+                  <div className="mt-4 mx-2">
+                    <div className="flex gap-4">
+                      <img
+                        src={card.companyProfilePicture}
+                        alt=""
+                        className="rounded-[10px] w-12 h-12"
+                      />
+                      <div>
+                        <h2 className="text-xl font-semibold text-[27px] text-center">
+                          {card.title}
+                        </h2>
+                        <p className="font-bold text-center">{card.service}</p>
+                      </div>
+                    </div>
+                    <h1 className="text-center font-bold text-[50px]">
+                      {card.ssdNo}
+                    </h1>
+                  </div>
+                </div>
+              ))}
+            </div>
+          </div>
+        )}
+      </div>
+    </div>
+  );
 }
 
 export default Home;
