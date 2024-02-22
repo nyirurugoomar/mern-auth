d0c000f32a61c09f8c3ae9e24921ec90dc7541ef
nyirurugo omar <nyirurugoomar@gmail.com>
2024-02-22 14:58:13 +0200

    update api integration home page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -5,6 +5,9 @@ import axios from "axios";
 function Home() {
   const [cards, setCards] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [updateData, setUpdateData] = useState({});
+  const [showUpdateModal, setShowUpdateModal] = useState(false);
+  const [selectedCardID, setSelectedCardID] = useState(null);
 
   useEffect(() => {
     const fetchCards = async () => {
@@ -30,6 +33,36 @@ function Home() {
     } catch (error) {
       console.error("Error deleting card:", error);
     }
+  };
+
+  const handleUpdate = (cardID) => {
+    const cardToUpdate = cards.find((card) => card._id === cardID);
+    setUpdateData(cardToUpdate);
+    setSelectedCardID(cardID);
+    setShowUpdateModal(true);
+  };
+
+  const handleSubmitUpdate = async () => {
+    try {
+      await axios.put(
+        `http://localhost:5173/api/card/cards/${selectedCardID}`,
+        updateData
+      );
+      const response = await axios.get(
+        "http://localhost:5173/api/card/getCards"
+      );
+      setCards(response.data);
+      setShowUpdateModal(false);
+      setUpdateData({});
+      setSelectedCardID(null);
+    } catch (error) {
+      console.error("Error updating card:", error);
+    }
+  };
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setUpdateData({ ...updateData, [name]: value });
   };
 
   return (
@@ -59,7 +92,7 @@ function Home() {
                         className="rounded-[10px] w-12 h-12"
                       />
                       <div>
-                        <h2 className="text-xl font-semibold text-[27px] text-center">
+                        <h2 className="text-xl font-semibold text-[2rem] text-center">
                           {card.title}
                         </h2>
                         <p className="font-bold text-center">{card.service}</p>
@@ -75,7 +108,10 @@ function Home() {
                       >
                         Delete
                       </button>
-                      <button className=" text-gray px-4 py-2 mt-2 rounded-md">
+                      <button
+                        onClick={() => handleUpdate(card._id)}
+                        className=" text-gray px-4 py-2 mt-2 rounded-md"
+                      >
                         Update
                       </button>
                     </div>
@@ -86,6 +122,55 @@ function Home() {
           </div>
         )}
       </div>
+      {/* Update Card Modal */}
+      {showUpdateModal && (
+        <div className="fixed inset-0 flex justify-center items-center bg-gray-900 bg-opacity-50 z-50">
+          <div className="bg-white p-8 rounded-md">
+            <h2 className="text-xl font-semibold mb-2 text-center">
+              Update Card
+            </h2>
+            <h4 className="font-bold">Title</h4>
+            <input
+              type="text"
+              name="title"
+              value={updateData.title || ""}
+              onChange={handleChange}
+              className="bg-slate-100 rounded-lg p-3 w-full"
+              placeholder="Title"
+            />
+            <h4 className="font-bold">Service</h4>
+            <input
+              type="text"
+              name="service"
+              value={updateData.service || ""}
+              onChange={handleChange}
+              className="bg-slate-100 rounded-lg p-3 w-full"
+              placeholder="Service"
+            />
+            <h4 className="font-bold">Number</h4>
+            <input
+              type="text"
+              name="ssdNo"
+              value={updateData.ssdNo || ""}
+              onChange={handleChange}
+              className="bg-slate-100 rounded-lg p-3 w-full mb-4"
+              placeholder="SSD No"
+            />
+            <button
+              onClick={handleSubmitUpdate}
+              className="bg-blue-800 text-white px-4 py-2 rounded-md"
+            >
+              Update
+            </button>
+            <button
+              onClick={() => setShowUpdateModal(false)}
+              className="bg-gray-500 text-white px-4 py-2 rounded-md ml-2"
+            >
+              Cancel
+            </button>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
