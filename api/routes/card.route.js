ee4fdf1dda8399977c6b5d325b0517d94fd2f162
nyirurugo omar <nyirurugoomar@gmail.com>
2024-02-22 15:05:41 +0200

    Setting verification token to Delete and Update

--- api/routes/card.route.js
+++ api/routes/card.route.js
@@ -5,11 +5,12 @@ import {
   updateCard,
   deleteCard,
 } from "../controllers/card.controller.js";
+import { verifyToken } from "../utils/VerifyUser.js";
 
 const router = express.Router();
 router.get("/getCards", getCards);
 router.post("/createCard", createCard);
-router.put("/cards/:cardID", updateCard);
-router.delete("/cards/:cardID", deleteCard);
+router.put("/cards/:cardID", verifyToken, updateCard);
+router.delete("/cards/:cardID", verifyToken, deleteCard);
 
 export default router;
