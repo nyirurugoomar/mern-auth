6e3d3b2c400f40e1beff456e8b288b5c9a655757
nyirurugo omar <nyirurugoomar@gmail.com>
2024-02-22 16:25:13 +0200

    adding pre production change

--- api/index.js
+++ api/index.js
@@ -4,7 +4,7 @@ import userRoutes from "./routes/user.route.js";
 import authRoutes from "./routes/auth.route.js";
 import cardRoutes from "./routes/card.route.js";
 import cookieParser from "cookie-parser";
-
+import path from "path";
 import dotenv from "dotenv";
 
 dotenv.config();
@@ -18,8 +18,16 @@ mongoose
     console.log(error);
   });
 
+const __dirname = path.resolve();
+
 const port = 3000;
 const app = express();
+
+app.use(express.static(path.join(__dirname, "/client/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+});
 
 app.use(express.json());
 app.use(cookieParser());
