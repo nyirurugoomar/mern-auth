d2137e7fe831e8c2474f38d6a4d074b00ec730e4
nyirurugoomar <nyirurugoomar@gmail.com>
2024-04-01 12:04:36 +0200

    changing link of api

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -14,7 +14,7 @@ function Home() {
     const fetchCards = async () => {
       try {
         const response = await axios.get(
-          "http://localhost:5173/api/card/getCards"
+          "https://mern-auth-original.onrender.com/api/card/getCards"
         );
         setCards(response.data);
         setLoading(false);
